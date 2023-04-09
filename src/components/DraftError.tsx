344aef5d5bb3d3502195d812b9f968b7c16457fb
Sawatdekrap <sawatdekrap@gmail.com>
2023-04-09 15:41:29 +1000

    Update draft error component rendering

--- src/components/DraftError.tsx
+++ src/components/DraftError.tsx
@@ -1,38 +1,74 @@
 import React from "react";
 import styled from "styled-components";
+import DraftCardItf from "../interfaces/draftCard";
 import DraftErrorItf from "../interfaces/draftError";
-import { FONT_TITLE, RED_DARK, RED_LIGHT } from "../styles";
+import { RED_DARK, RED_LIGHT } from "../styles";
 
 const DraftErrorWrapper = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  background-color: ${RED_DARK};
+  color: white;
+  padding: 4px 16px;
+  border-radius: 8px;
+  margin: 4px 0;
 `;
 
-const ErrorIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  margin: 4px;
-  border-radius: 50%;
+const DraftErrorDescription = styled.div``;
+
+const QuickFixWrapper = styled.div`
+  margin-top: 4px;
+`;
+
+const QuickFix = styled.button`
+  height: 100%;
+  border: none;
   background-color: ${RED_LIGHT};
   color: white;
-  line-height: 1.5;
-  font-family: ${FONT_TITLE};
-`;
+  margin: 0 8px;
+  font-weight: bold;
 
-const DraftErrorDescription = styled.div`
-  flex: auto;
-  text-align: left;
+  :hover {
+    opacity: 0.75;
+    cursor: pointer;
+  }
 `;
 
 interface DraftErrorProps {
   draftError: DraftErrorItf;
+  draftCards: DraftCardItf[];
+  setDraftCards: (newDraftCards: DraftCardItf[]) => void;
 }
 
-const DraftError = ({ draftError }: DraftErrorProps) => {
+const DraftError = ({
+  draftError,
+  draftCards,
+  setDraftCards,
+}: DraftErrorProps) => {
   return (
     <DraftErrorWrapper>
-      <ErrorIcon>!</ErrorIcon>
       <DraftErrorDescription>{draftError.description}</DraftErrorDescription>
+      {draftError.resolutions && (
+        <QuickFixWrapper>
+          {draftError.resolutions.map((resolution) => {
+            const applyResolution = () => {
+              const newDraftCards = resolution.apply(draftCards);
+              setDraftCards([...newDraftCards]);
+
+              const newDraftCardIds = newDraftCards
+                .filter((dc) => dc.count > 0)
+                .map((dc) => dc.card.id);
+              console.log(`New draft cards: ${newDraftCardIds}`);
+            };
+
+            return (
+              <QuickFix key={resolution.description} onClick={applyResolution}>
+                {resolution.description}
+              </QuickFix>
+            );
+          })}
+        </QuickFixWrapper>
+      )}
     </DraftErrorWrapper>
   );
 };
--- src/components/DraftErrors.tsx
+++ src/components/DraftErrors.tsx
@@ -1,34 +1,83 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import DraftCardItf from "../interfaces/draftCard";
 import DRAFT_ERRORS from "../constants/draftErrors";
 import styled from "styled-components";
 import DraftErrorItf from "../interfaces/draftError";
 import DraftError from "./DraftError";
-import { FONT_TITLE } from "../styles";
+import { FONT_TITLE, GREY_LIGHT, RED_LIGHT } from "../styles";
 
-const DraftErrorsTitle = styled.div`
-  font-family: ${FONT_TITLE};
-  font-size: 32px;
-  text-align: left;
+const DraftErrorsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const DraftErrorsWrapper = styled.div``;
+const ErrorIconWrapper = styled.div``;
+
+const ErrorIcon = styled.button`
+  width: 64px;
+  height: 64px;
+  margin: 4px;
+  border-radius: 50%;
+  background-color: ${RED_LIGHT};
+  color: white;
+  line-height: 1;
+  font-family: ${FONT_TITLE};
+  font-size: 56px;
+  border: none;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const ErrorCountWrapper = styled.div`
+  position: absolute;
+`;
+
+const ErrorCountIcon = styled.div`
+  position: absolute;
+  left: 48px;
+  width: 20px;
+  height: 20px;
+  background-color: ${GREY_LIGHT};
+  border-radius: 50%;
+  color: white;
+`;
 
 interface DraftErrorsProps {
   draftCards: DraftCardItf[];
+  setDraftCards: (newDraftCards: DraftCardItf[]) => void;
 }
 
-const DraftErrors = ({ draftCards }: DraftErrorsProps) => {
+const DraftErrors = ({ draftCards, setDraftCards }: DraftErrorsProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const activeErrors = useMemo<DraftErrorItf[]>(() => {
     return DRAFT_ERRORS.filter((de) => de.isError(draftCards));
   }, [draftCards]);
 
+  const nErrors = activeErrors.length;
+  if (nErrors === 0) return null;
+
   return (
     <DraftErrorsWrapper>
-      <DraftErrorsTitle>Errors</DraftErrorsTitle>
-      {activeErrors.map((draftError) => (
-        <DraftError key={draftError.description} draftError={draftError} />
-      ))}
+      <ErrorIconWrapper>
+        <ErrorCountWrapper>
+          <ErrorCountIcon>{nErrors}</ErrorCountIcon>
+        </ErrorCountWrapper>
+        <ErrorIcon onClick={() => setIsExpanded(!isExpanded)}>!</ErrorIcon>
+      </ErrorIconWrapper>
+
+      {isExpanded &&
+        activeErrors.map((draftError) => (
+          <DraftError
+            key={draftError.description}
+            draftError={draftError}
+            draftCards={draftCards}
+            setDraftCards={setDraftCards}
+          />
+        ))}
     </DraftErrorsWrapper>
   );
 };
