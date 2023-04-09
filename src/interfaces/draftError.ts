59bf99b3dd7fc9939e1b6d9abacf01c41b8a537e
Sawatdekrap <sawatdekrap@gmail.com>
2023-04-09 15:39:17 +1000

    Add optional resolutions for draftErrors

--- src/interfaces/draftError.ts
+++ src/interfaces/draftError.ts
@@ -1,6 +1,12 @@
 import DraftCardItf from "../interfaces/draftCard";
 
+export interface DraftErrorResolutionItf {
+  description: string;
+  apply: (draftCards: DraftCardItf[]) => DraftCardItf[];
+}
+
 export default interface DraftErrorItf {
   description: string;
   isError: (draftCards: DraftCardItf[]) => boolean;
+  resolutions?: DraftErrorResolutionItf[];
 }
